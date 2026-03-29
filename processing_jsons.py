4a496c838ab3da807ca545159714bd3e4ebc128a
Tatyana Khakhaleva <tkhakhaleva@gmail.com>
2026-03-29 15:44:31 +0300

    Update processing_jsons.py

--- processing_jsons.py
+++ processing_jsons.py
@@ -1,43 +1,61 @@
 import os, json
 import pandas as pd
 
-# set format of the files   
-path_to_json = '/home/tanya/Programming/Projects/temp/pastvu_photos'
+path_to_json = '/home/tanya/Programming/Projects/temp/Pastvu/pastvu_photos'
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+all_data = []
 
-# enumerate required columns 
-jsons_data = pd.DataFrame(columns=['login', 'sex', 'name', 'lon', 'lat', 'year', 'country', 'city'])
-
-
-# SAVING INFORMATION 
+# PROCESSING JSONs
 for index, js in enumerate(json_files):
-    with open(os.path.join(path_to_json, js)) as json_file:
-        json_text = json.load(json_file) 
-    
     try:
-        if "title" not in json_text:
-            continue
-            
-        if "y" not in json_text:
-            continue
-            
-        if "regions" not in json_text or len(json_text["regions"]) == 0:
-            continue
+        with open(os.path.join(path_to_json, js), 'r', encoding='utf-8') as json_file:
+            json_text = json.load(json_file)
+        photo_data = json_text.get("result", {}).get("photo", {})
         
-        login = json_text["result"]["photo"]["user"]["login"]
-        sex = json_text["result"]["photo"]["user"]["sex"]
-        name = json_text["result"]["photo"]["title"]
-        lat = json_text["result"]["photo"]["geo"][1]
-        lon = json_text["result"]["photo"]["geo"][0]
-        year = json_text["result"]["photo"]["y"]
-        country = json_text["result"]["photo"]["regions"][0]["title_local"]
-        city = json_text["result"]["photo"]["regions"][1]["title_local"]
-        
-        jsons_data.loc[index] = [login, sex, name, lat, lon, year, country, city]
-        
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
+        if not photo_data:
+            continue
 
-#jsons_data.to_csv('PastVu_database.csv', index=True)
-print(jsons_data)
+        if not all(key in photo_data for key in ["title", "y", "regions", "geo", "user"]):
+            continue
+
+        # getting data 
+        login = photo_data["user"].get("login", "")
+        sex = photo_data["user"].get("sex", "")
+        name = photo_data.get("title", "")
+        geo = photo_data.get("geo", [])
+        if len(geo) >= 2:
+            lon = geo[1]
+            lat = geo[0]
+        else:
+            continue 
+        year = photo_data.get("y", "")
+        regions = photo_data.get("regions", [])
+        country = regions[0].get("title_local", "") if len(regions) > 0 else ""
+        city = regions[1].get("title_local", "") if len(regions) > 1 else ""
+        
+        all_data.append({
+            'login': login,
+            'sex': sex,
+            'name': name,
+            'lon': lon,
+            'lat': lat,
+            'year': year,
+            'country': country,
+            'city': city,
+            'filename': js  # опционально: имя файла для отслеживания
+        })
+            
+    except Exception as e:
+        # print(f"Error for {js}: {str(e)[:50]}...")
+        continue  # просто пропускаем проблемные файлы
+
+# SAVING
+if all_data:
+    jsons_data = pd.DataFrame(all_data)
+    print(jsons_data.head())
+
+    output_path = '/home/tanya/Programming/Projects/temp/Pastvu/PastVu_database.csv'
+    jsons_data.to_csv(output_path, index=False, encoding='utf-8')
+    
+else:
+    print("Processing failed.")
